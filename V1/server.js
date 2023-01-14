9628a872148a205facbb89b1608fc43c534c9506
Muhammad Nasir <muhnasir@students.uit.edu>
2023-01-14 16:51:12 +0500

    Checking for API functionality

--- V1/server.js
+++ V1/server.js
@@ -14,51 +14,37 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'user_test',
-        insecureAuth : true,
-      });
-      
-      connection.connect((error) => {
-        if(error) {
-          console.log('Error connecting: ' + error.message);
-          return;
-      }
-      console.log('Connection established sucessfully');
-      });
 app.get('/api/login', (req, res) => {
     // res.addHeader("Access-Control-Allow-Origin", "*");
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'user_test',
-        insecureAuth : true,
-      });
+//     const connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'password',
+//         database: 'user_test',
+//         insecureAuth : true,
+//       });
       
-      connection.connect((error) => {
-        if(error) {
-          console.log('Error connecting: ' + error.message);
-          return;
-      }
-      console.log('Connection established sucessfully');
-      connection.query("SELECT * from users", function (err, result) {
-          if (err) {
-              console.log('Error on query: ' + err.message);
-              return;
-          }
-          console.log("Query Successful");
-          console.log(result);
-          const dbRes = { 
-            username: result[0].username,
-            password: result[0].password,
-        };
-          res.send(dbRes);
-        });
-    });
+//       connection.connect((error) => {
+//         if(error) {
+//           console.log('Error connecting: ' + error.message);
+//           return;
+//       }
+//       console.log('Connection established sucessfully');
+//       connection.query("SELECT * from users", function (err, result) {
+//           if (err) {
+//               console.log('Error on query: ' + err.message);
+//               return;
+//           }
+//           console.log("Query Successful");
+//           console.log(result);
+//           const dbRes = { 
+//             username: result[0].username,
+//             password: result[0].password,
+//         };
+//           res.send(dbRes);
+//         });
+//     });
+       res.send("HELLO WORLD");
 })
 
 app.post('/api/signup', function (req, res) {
