13f4b518463a2ef560a21ba1a267ce810fcccbe3
Muhammad Nasir <muhnasir@students.uit.edu>
2023-05-01 11:47:20 +0500

    commented return on query error

--- V1/server.js
+++ V1/server.js
@@ -79,20 +79,20 @@ app.post('/api/login', (req, res) => {
     }
     console.log('Connection: Established sucessfully'); 
     });
-//     connection.query("SELECT * from users", function (err, result) {
-//         if (err) {
-//             console.log('Error on query: ' + err.message);
+    connection.query("SELECT * from users", function (err, result) {
+        if (err) {
+            console.log('Error on query: ' + err.message);
 //             return;
-//         }
-//         console.log("Query: Successful");
-//         const records = result;
-//         const recordsSearch = records.find(item => item.username === loginUser && item.password === loginPass);
-//         let ans = false;
-//         if (recordsSearch) ans = true;
-//         console.log("Authentication: Complete!")
-//         res.send(ans);
-//       });
-      connection.end()
+        }
+        console.log("Query: Successful");
+        const records = result;
+        const recordsSearch = records.find(item => item.username === loginUser && item.password === loginPass);
+        let ans = false;
+        if (recordsSearch) ans = true;
+        console.log("Authentication: Complete!")
+        res.send(ans);
+      });
+//       connection.end()
 })
 
 app.post('/api/signup', function (req, res) {
